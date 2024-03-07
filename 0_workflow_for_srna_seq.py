69e3eba21fdfe35c6d2a01179917e3a46f95dd53
Yuchen@fdu <chenyc2048@outlook.com>
2024-03-07 16:23:22 +0800

    ✨ feat: add adapter.
    add auto detect adapter to adjust public library.

--- 0_workflow_for_srna_seq.py
+++ 0_workflow_for_srna_seq.py
@@ -19,7 +19,12 @@ from Bio.SeqRecord import SeqRecord
 
 
 def data_trim():
-    trim_cmd = ["trim_galore", "--fastqc", "--gzip", "--length", "10", "--trim-n", "--suppress_warn", "-j", "12", "-o", trim_path, input_path + "/" + key + ".fastq.gz"]
+    adapter_search = subprocess.run(["/bios-store1/chenyc/scripts/Github_scripts/DNApi/dnapi.py", input_path + "/" + key + ".fastq.gz"], check=True, stdout=subprocess.PIPE, text=True)
+    adapter = adapter_search.stdout.split()[0]
+    trim_cmd = ["trim_galore", "--fastqc", 
+                "-a", adapter,
+                "--gzip", "--length", "10", "--max_length", "35", "--consider_already_trimmed", "10",
+                "--trim-n", "--suppress_warn", "-j", "12", "-o", trim_path, input_path + "/" + key + ".fastq.gz"]
     subprocess.run(trim_cmd, check=True)
 
 def umi_extract():
@@ -64,7 +69,6 @@ def data_length_dist():
 def data_mapping():
     bowtie_fq_cmd = [
     "bowtie",
-    "-f",
     "-p", "12",
     "-m", "50",
     "-v", "0",
@@ -74,7 +78,7 @@ def data_mapping():
     "--no-unal",
     "--shmem",
     "-x", genomeindex, 
-    umi_path + "/" + key + '_trimmed.fa.gz',
+    umi_path + "/" + key + '_trimmed.fq.gz',
     "-S", mapping_path + '/' + key + '_aligned.sam',
     "--al", mapping_path + '/' + key + '_aligned.fa'
     ]
@@ -91,22 +95,23 @@ def convert_bam2bigwig():
     pysam.index(bamfile)
     os.remove(samfile)
 
-    bw_cmd = [
-    "ShortTracks",
-    "--bamfile", bamfile,
-    "--mode", "readlength",
-    "--stranded"
-    ]
-    subprocess.run(bw_cmd, check=True)
-    bw_files = glob.glob(mapping_path + '/*.bw')
-    for bw_file in bw_files:
-        shutil.move(bw_file, doc_path)
+    # bw_cmd = [
+    # "ShortTracks",
+    # "--bamfile", bamfile,
+    # "--mode", "readlength",
+    # "--stranded"
+    # ]
+    # subprocess.run(bw_cmd, check=True)
+    # bw_files = glob.glob(mapping_path + '/*.bw')
+    # for bw_file in bw_files:
+    #     shutil.move(bw_file, doc_path)
 
 if __name__ == "__main__":
 
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     doc_path = sys.argv[2]
+    flag = False
     fastq_files = glob.glob(sys.argv[1] + "/*.fastq.gz", recursive=True)
     trim_path = os.path.join(output_path, "2_trim_adapter/")
     umi_path = os.path.join(output_path, "3_extract_umi/")
@@ -120,16 +125,19 @@ if __name__ == "__main__":
         data_trim()
 
         # step2: extract umi
-        if not os.path.exists(umi_path):
-            os.mkdir(umi_path)
-        umi_extract()
+        if flag == True:
+            if not os.path.exists(umi_path):
+                os.mkdir(umi_path)
+            umi_extract()
+        else:
+            umi_path = trim_path
 
         # step3: mapping
         if not os.path.exists(mapping_path):
             os.mkdir(mapping_path)
-        genomeindex = "/bios-store1/chenyc/Project/multispecies/physcomitrium_patens/0_genome/dna_index/Physcomitrium_patens.Phypa_V3.dna.toplevel"
+        genomeindex = "/bios-store1/chenyc/Reference_Source/Arabidopsis_Reference/ath_chr_bowtie_index/Arabidopsis_thaliana.TAIR10.dna.toplevel"
         data_mapping()
 
         # step4: data length distribution
-        data_length_dist()
+        # data_length_dist()
         convert_bam2bigwig()
